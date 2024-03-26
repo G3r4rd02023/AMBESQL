d79eb5d923300e4e916a3dfa1254bdc8ad766e8e
Gerardo Lanza <glanza007@gmail.com>
2024-03-26 17:56:16 -0600

    unidades

--- ProyectoAMBE/Controllers/UnidadesController.cs
+++ ProyectoAMBE/Controllers/UnidadesController.cs
@@ -16,23 +16,34 @@ namespace ProyectoAMBE.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Unidades>>> ObtenerUnidades(int idInstituto)
+        public async Task<ActionResult<IEnumerable<Unidades>>> ObtenerUnidades()
         {
             if (_context.Unidades == null)
             {
                 return NotFound();
             }
 
-            var marcas = await _context.Unidades
-                .Where(m => m.IdInstituto == idInstituto)
+            var unidades = await _context.Unidades                
                 .ToListAsync();
-            return Ok(marcas);
+            return Ok(unidades);
         }
 
         [HttpPost]
         public async Task<ActionResult<Unidades>> CrearUnidad(Unidades unidad)
         {
             await _context.Unidades.AddAsync(unidad);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditarUnidad(int id, Unidades unidad)
+        {
+            if (id != unidad.IdUnidad)
+            {
+                return BadRequest();
+            }
+            _context.Entry(unidad).State = EntityState.Modified;
             await _context.SaveChangesAsync();
             return Ok();
         }
